de89cda20ee4273ef4f72190aca658bbe9240ccc
Kevin Cox <jakobcox@me.com>
2025-04-16 06:35:42 -0500

    Initial Commit

---
+++ Taskey.Server/Taskey.Server.WebAPI/Controllers/InfoController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Taskey.Server.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InfoController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetInfo()
+        {
+            var info = new
+            {
+                Version = "1.0.0",
+                Description = "Taskey API is a simple task tracker API."
+            };
+            return Ok(info);
+        }
+    }
+}
